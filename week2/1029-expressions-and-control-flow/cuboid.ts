e5a6634185d763eff4ad2c67ea58d8ff494a60ca
giuliette <domanyjuli@gmail.com>
2018-10-29 14:27:35 +0100

    cuboid calculations as cube

---
+++ week2/1029-expressions-and-control-flow/cuboid.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+// Write a program that stores 3 sides of a cuboid as variables (floats)
+// The program should write the surface area and volume of the cuboid like:
+//
+// Surface Area: 600
+// Volume: 1000
+
+let a: number = 10;
+
+console.log("surface area: " + a * a * 6);
+console.log("volume: " + a ** 3);
