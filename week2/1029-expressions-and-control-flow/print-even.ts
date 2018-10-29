a3246a93d2825099849db2552f57d43492ae125d
giuliette <domanyjuli@gmail.com>
2018-10-29 16:24:12 +0100

    print-even

---
+++ week2/1029-expressions-and-control-flow/print-even.ts
@@ -0,0 +1,10 @@
+'use strict';
+export{}
+
+// Create a program that prints all the even numbers between 0 and 500
+
+for (let j: number = 0; j <= 500; j++) {
+    if(j % 2 === 0)
+    console.log(j);
+  }
+
