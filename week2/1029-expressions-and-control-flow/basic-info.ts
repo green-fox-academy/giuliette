4d56434fb97dd63d9a97d53538f119bf70c257da
giuliette <domanyjuli@gmail.com>
2018-10-29 13:02:22 +0100

    basic-info no spaces

---
+++ week2/1029-expressions-and-control-flow/basic-info.ts
@@ -0,0 +1,16 @@
+export {}
+
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+let myName: string ="Juli";
+// Your age as an integer
+let a: number = 10;
+// Your height in meters as a float
+let h: number = 1.72;
+// Whether you are married or not as a boolean
+let maritalStatus: boolean = (false);
+
+
+console.log(myName + a + h + maritalStatus);
