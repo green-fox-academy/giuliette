3fa813466ddade412bdc53f30f9b08a5f3243295
giuliette <domanyjuli@gmail.com>
2018-10-29 12:20:08 +0100

    favorite no second commit, first failed

---
+++ week2/1029-expressions-and-control-flow/fav-number.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+let favoriteNumber: number = 13;
+let txt: string = "My favorite number is: "
+
+// Store your favorite number in a variable (as a number)
+// And print it like this: 'My favorite number is: 8'
+
+console.log("favoriteNumber" + "txt");
---
+++ week2/1029-expressions-and-control-flow/swap.ts
@@ -0,0 +1,13 @@
+
+'use strict';
+
+// Swap the values of these variables
+let a: number = 123;
+let b: number = 526;
+
+a = 526;
+b = 123;
+
+console.log(a);
+console.log(b);
+
