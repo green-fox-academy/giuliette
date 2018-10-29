e0bd585d9ea6b5a4248d15a0613bb6d03c145a96
giuliette <domanyjuli@gmail.com>
2018-10-29 16:03:19 +0100

    i will cheat on the exams

---
+++ week2/1029-expressions-and-control-flow/i-wont-cheat-on-the-exams.ts
@@ -0,0 +1,13 @@
+'use strict';
+export{}
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+let a: number = 0;
+while (a < 100){
+    console.log("I won't cheat on the exam!");
+    a++;
+}
+
+
