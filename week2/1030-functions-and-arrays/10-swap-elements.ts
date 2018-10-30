aa25ff3cf5dc0e2340256e57c599742f88660c1a
giuliette <domanyjuli@gmail.com>
2018-10-30 15:50:27 +0100

    change element from 8 to 4

---
+++ week2/1030-functions-and-arrays/10-swap-elements.ts
@@ -0,0 +1,9 @@
+'use strict';
+export{};
+
+// -  Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// -  Swap the first and the third element of `abc`
+
+let abc:string[] = ["Arthur", "Boe", "Chloe"];
+let reversed = abc.reverse(); 
+console.log(reversed);
---
+++ week2/1030-functions-and-arrays/12-change-element.ts
@@ -0,0 +1,21 @@
+// -  Create an array named `numList` with the following content: `[1, 2, 3, 8, 5, 6]`
+// -  Change the 8 to 4 with the `.map` method 
+// -  Print the fourth element as a test
+
+
+/*let valami = numList 
+
+let kiskutya = numList.map(function(element)) { 
+    if(element === 8; return: 4;)
+return Element;
+}
+*/
+
+let numList: number[] = [1, 2, 3, 8, 5, 6];
+let change: number[] = numList.map(function (e) {
+    if (e == 8) {
+        return 4;
+    } else return e;
+});
+
+console.log(change);
