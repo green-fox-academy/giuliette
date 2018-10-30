a373300c02a6ec327ee1fef7dbc180cfeaf5f58c
giuliette <domanyjuli@gmail.com>
2018-10-30 14:48:33 +0100

    comparison of list lenghts, prints only if first has more elements

---
+++ week2/1030-functions-and-arrays/8-comparelengths.ts
@@ -0,0 +1,18 @@
+
+'use strict';
+export{};
+
+// -  Create a variable named `firstList`
+//    with the following content: `[1, 2, 3]`
+// -  Create a variable named `secondList`
+//    with the following content: `[4, 5]`
+// -  Log to the console if `secondList` has more elements than `firstList`
+
+var firstList = [1, 2, 3];
+var secondList = [4, 5];
+
+if (firstList.length > secondList.length){
+    console.log(firstList);   
+} 
+
+
