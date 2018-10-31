046223ad44ffc42dfb206fad729fc07c1f620889
giuliette <domanyjuli@gmail.com>
2018-10-31 10:02:15 +0100

    diamond

---
+++ week2/1029-expressions-and-control-flow/diamond.ts
@@ -0,0 +1,17 @@
+'use strict';
+export{};
+
+let lineCount: number = 7;
+
+// Write a program that draws a
+// diamond like this:
+//
+//    *
+//   ***
+//  *****
+// *******
+//  *****
+//   ***
+//    *
+//
+// The diamond should have as many lines as lineCount is
