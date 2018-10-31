d0efa33cae9c683dcb5ffb8673aa985fe7fc1d43
giuliette <domanyjuli@gmail.com>
2018-10-31 09:56:33 +0100

    matrix task undone, cleaning working tree

---
+++ week2/1030-functions-and-arrays/18-matrix.ts
@@ -0,0 +1,17 @@
+'use strict';
+export{};
+
+// -  Create (dynamically*) a two dimensional list
+//    with the following matrix**. Use a loop!
+//
+//    0 0 0 1
+//    0 0 1 0
+//    0 1 0 0
+//    1 0 0 0
+//
+// -  Print this two dimensional list to the console
+//
+// * size should depend on a variable
+// ** Relax, a matrix is just like an array
+
+let lineCount:number = 4;
