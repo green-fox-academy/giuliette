ef8098ed06c63a18d6fc0f1deb9d1a962f7bf077
giuliette <domanyjuli@gmail.com>
2018-11-07 15:53:03 +0100

    purplesteps 3d

---
+++ week3/1007-drawing/12-purplesteps3d/purplesteps.ts
@@ -0,0 +1,35 @@
+
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Reproduce this:
+// [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
+/* first try 
+function rectangles(x, y, a) {
+  ctx.fillStyle = 'purple';
+  ctx.fillRect(x, y, x, x);
+}
+
+let square 
+for (let i = 1; i < 6; i++) {
+  rectangles(Math.sqrt(i),Math.sqrt(i));
+}
+*/
+
+let start: number = 10;
+let addstart: number = 5;
+
+let x: number = 10;
+let y: number = 10;
+
+for (let i: number = 0; i < 6; i++) {
+  ctx.fillStyle = "purple";
+  ctx.fillRect(x, y, start, start);
+  x += start;
+  y += start;
+  start += addstart;
+}
