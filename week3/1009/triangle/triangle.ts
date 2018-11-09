485030cff71d18f832565f39aecb51aa3ddf6473
giuliette <domanyjuli@gmail.com>
2018-11-09 14:06:29 +0100

    triangles per row

--- week3/1009/triangle/triangle.ts
+++ week3/1009/triangle/triangle.ts
@@ -3,18 +3,21 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-function drawTriangle(x: number, y: number):void {
-  ctx.strokeStyle = 'red';
+let side = 100;
+let triangleHeight = side + 0;
+
+function drawTriangle(x: number, y: number): void {
   ctx.beginPath();
-  ctx.moveTo(x, y); //x,y
-  ctx.lineTo(x + 50, y); //x+50,y
-  ctx.lineTo(x + 25, y - 50); //x*1.5, y-50
-  ctx.lineTo(x, y); //x, y
+  ctx.moveTo(x, y + side);
+  ctx.lineTo(x + side, y + side);
+  ctx.lineTo(x + side / 2, y)
+  ctx.lineTo(x, y + side);
   ctx.stroke();
 }
 
-for (let i = 0; i < canvas.width; i += 50) {
-  for (let j = 0; j < canvas.height; j += 50) {
-    drawTriangle(i, j);
+for (let i = 0; i < canvas.width / side; i++) {
+  for (let j = i; j < canvas.height / triangleHeight; j++) {
+    drawTriangle(i * side, j * side)
   }
 }
+
