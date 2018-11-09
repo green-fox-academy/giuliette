f348826edae0da0df0ea236e86ac7e2e105dbe37
giuliette <domanyjuli@gmail.com>
2018-11-09 10:09:59 +0100

    lineplay

--- week3/1009/lineplay/lineplay.ts
+++ week3/1009/lineplay/lineplay.ts
@@ -3,9 +3,10 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
+
 for (let i = 10; i < 600; i += 10) {
   let x = i;
-  let y = 400 - i;
+  let y = 400 - x;
   ctx.strokeStyle = 'green';
   ctx.beginPath();
   ctx.moveTo(x, 0);
