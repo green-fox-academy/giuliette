5c3eabb1b738cb14a1a669e12e1805d4fcc48917
giuliette <domanyjuli@gmail.com>
2018-11-12 16:05:57 +0100

    write name into txt file with error msg

---
+++ week3/1112-file-IO/04-write-single-line.ts
@@ -0,0 +1,23 @@
+import { write } from "fs";
+
+// Open a file called 'my-file.txt'
+// Write your name in it as a single line
+// If the program is unable to write the file,
+// then it should print an error message like: 'Unable to write file: my-file.txt'
+
+export { };
+const fs = require('fs');
+
+
+function writeToMyFile(fileName: string, data: string): void {
+  //  fs.writeFileSync(fileName, data);
+  if (fs.existsSync(fileName)) {
+    fs.appendFileSync(fileName, data);
+    console.log('kiskutza');
+  } 
+  else {
+    console.log(`Unable to write file: ${fileName}`);
+  }
+}
+
+writeToMyFile('valamike.txt', 'Julika')
