e9bb119a2f32676d3ff2bbf151521b198d819ee1
giuliette <domanyjuli@gmail.com>
2018-11-13 11:06:01 +0100

    style appended

--- week4/1113-oop/02-blogpost.ts
+++ week4/1113-oop/02-blogpost.ts
@@ -36,7 +36,6 @@ var blogPostOne = new blogPost ('John Doe', 'Lorem Ipsum', 'Lorem ipsum dolor si
 var blogPostTwo = new blogPost ('Tim Urban', 'Wait but why', 'A popular long-form, stick-figure-illustrated blog about almost everything.', '2010.10.10.');
 var blogPostThree = new blogPost ('William Turton', 'One Engineer Is Trzing to Get IBM to Reckon with Trump', ' Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.', '2017.03.28.')
 
-
 console.log(blogPostOne);
 console.log(blogPostTwo);
 console.log(blogPostThree);
