e9113d824db15f8481662812c1b9d40bb0e27329
giuliette <domanyjuli@gmail.com>
2018-11-13 11:17:44 +0100

    animals in the meantime

---
+++ week4/1113-oop/03-animal.ts
@@ -0,0 +1,29 @@
+// Create an Animal class
+// Every animal has a hunger value, which is a whole number
+// Every animal has a thirst value, which is a whole number
+// when creating a new animal object these values are created with the default 50 value
+// Every animal can eat() which decreases their hunger by one
+// Every animal can drink() which decreases their thirst by one
+// Every animal can play() which increases both by one
+
+export { };
+
+class animal {
+  hungerValue: number;
+  thirstValue: number;
+  constructor(hungerV: number = 50, thirstV: number = 50){
+    this.hungerValue = hungerV;
+    this.thirstValue = thirstV;
+  }
+  eat(): any {
+    return this.hungerValue + 1;
+  }
+  drink(): any {
+    return this.thirstValue + 1;
+  }
+  play(): any {
+    return this.thirstValue - 1, this.hungerValue - 1;
+  }
+}
+
+
