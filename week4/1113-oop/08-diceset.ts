4323d1aafd531fe10e32dfb4fd25d2641d193f99
giuliette <domanyjuli@gmail.com>
2018-11-13 19:27:18 +0100

    dices

---
+++ week4/1113-oop/08-diceset.ts
@@ -0,0 +1,49 @@
+class DiceSet {
+  dices: number[];
+  readonly numOfDices: number = 6;
+
+  roll(): number[] {
+    this.dices = [];
+    for (var i = 0; i < this.numOfDices; i++) {
+      this.dices.push(Math.floor(Math.random() * 6 + 1));
+    }
+    return this.dices;
+  }
+
+  reroll(index?: number) {
+    if (index == undefined) {
+      for (var i = 0; i < this.numOfDices; i++) {
+        this.dices[i] = Math.floor(Math.random() * 6 + 1);
+      }
+    } else {
+      this.dices[index] = Math.floor(Math.random() * 6 + 1);
+    }
+  }
+
+  getCurrent(index?: number) {
+    if (index == undefined) {
+      for (var i = 0; i < this.numOfDices; i++) {
+        console.log(this.dices[i]);
+      }
+    } else {
+      console.log(this.dices[index]);
+    }
+  }
+}
+
+
+let diceSet = new DiceSet();
+
+diceSet.roll();
+
+//diceSet.getCurrent();
+
+while (!diceSet.dices.every(e => e === 6)) {
+  for (let i = 0; i < 6; i++) {
+    if (diceSet.dices[i] !== 6) {
+      diceSet.reroll(i);
+    } 
+  }
+}
+
+diceSet.getCurrent();
