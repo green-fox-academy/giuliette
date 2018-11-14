37058cdbc61aa69546abc6265d517234f953b4e4
giuliette <domanyjuli@gmail.com>
2018-11-14 15:24:42 +0100

    practice

---
+++ week4/1114-practice/10-dominoes/domino.ts
@@ -0,0 +1,5 @@
+// Dominoes
+// You have the list of Dominoes
+// Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
+// eg: [2, 4], [4, 3], [3, 5] ...
+
---
+++ week4/1114-practice/14-blog/blog.ts
@@ -0,0 +1,22 @@
+// Blog
+// Reuse your BlogPost class
+// Create a Blog class which can
+// store a list of BlogPosts
+// add BlogPosts to the list
+
+// delete(int) one item at given index
+// update(int, BlogPost) one item at the given index and update it with another BlogPost
+
+import { BlogPost } from './BlogPost';
+
+class Blog {
+  //properties
+  listOfBlogPosts: BlogPost[]; //blogpost type elements
+
+  //method: adds new blogpost to the list
+
+  //method: splice from blogposts
+  //method: update new
+
+  
+}
