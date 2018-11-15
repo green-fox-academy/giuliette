508f32716ffe04c051f075c4dbef673bb1c54982
giuliette <domanyjuli@gmail.com>
2018-11-15 12:17:25 +0100

    recursion: sum digits of a number

--- week4/1115-recursion/sum-digit.ts
+++ week4/1115-recursion/sum-digit.ts
@@ -4,8 +4,15 @@
 
 function sumDigits(n: number): number {
   if (n > 0) {
-    return % n; 
+    return Math.floor(n % 10 + sumDigits(n / 10)); 
+    // 6  n % 10 
+    // 3  (n / 10) % 10,  
+    // 1  ((n / 10) / 10) % 10
   }
+  return 0;
 }
 
-console.log(sumDigits(111));
+console.log(sumDigits(1352));
+
+
+
