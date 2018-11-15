fc59bc628b9448685c5bd3f8d6bac2a302508f84
giuliette <domanyjuli@gmail.com>
2018-11-15 11:10:54 +0100

    recursive number adding

---
+++ week4/1115-recursion/number-adder.ts
@@ -0,0 +1,11 @@
+// Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+
+function numberAdder(n: number): number {
+  if (n <= 1) {
+    return n;
+  } else {
+    return n + numberAdder(n - 1);
+  }
+}
+
+console.log(numberAdder(5));
