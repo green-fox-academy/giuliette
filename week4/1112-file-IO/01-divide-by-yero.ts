60a33401fe6ed64824bf26a86c7c94e2f61332bb
giuliette <domanyjuli@gmail.com>
2018-11-19 11:57:19 +0100

    feat(homework): new person class

---
+++ week4/1112-file-IO/01-divide-by-yero.ts
@@ -0,0 +1,20 @@
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print 'fail' if the parameter is 0
+
+
+function divide(a: number, b: number): number {
+  if (b === 0) {
+    throw new TypeError('FAIL: Can\'t divide by zero');
+  }
+  return a / b;
+}
+
+try {
+  console.log(divide(10, 0));
+} catch (e) {
+  console.log(e.message);
+} finally {
+  console.log('cica');
+}
