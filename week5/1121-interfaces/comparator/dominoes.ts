72ab218897264226502a43d8d66eb565f31349df
giuliette <domanyjuli@gmail.com>
2018-11-21 11:31:55 +0100

    feat(Comparator): dominoes ordered based on first number

---
+++ week5/1121-interfaces/comparator/dominoes.ts
@@ -0,0 +1,33 @@
+'use strict'
+import {Domino} from "./domino";
+
+function initializeDominoes(): Domino[] {
+    let dominoes = [];
+    dominoes.push(new Domino(5, 2));
+    dominoes.push(new Domino(4, 6));
+    dominoes.push(new Domino(1, 5));
+    dominoes.push(new Domino(6, 7));
+    dominoes.push(new Domino(2 ,4));
+    dominoes.push(new Domino(7, 1));
+    return dominoes;
+}
+
+function print(dominoes: Domino[]) {
+    dominoes.forEach(function (value) {
+        console.log(value);
+    });
+}
+
+let dominoes = initializeDominoes();
+/** You have the list of Dominoes */
+/** Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides */
+/** eg: [2, 4], [4, 3], [3, 5] ... */
+
+//sort 
+dominoes.sort(function(a: Domino, b: Domino) {
+  return a.compareTo(b);
+})
+
+
+print(dominoes);
+
