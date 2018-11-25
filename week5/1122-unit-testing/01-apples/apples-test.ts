41cfc9ea453246490505925fd56d8efe74e99f0b
giuliette <domanyjuli@gmail.com>
2018-11-25 12:53:32 +0100

    feat(Apples): class Apple, returns string added

---
+++ week5/1122-unit-testing/01-apples/apples-test.ts
@@ -0,0 +1,14 @@
+'use strict'
+
+import  test  from 'tape';
+import { Apple } from './apples';
+
+test('check string', t => {
+  const golden = new Apple();
+
+  let actual = golden.getApple();
+  let expected = 'apple';
+
+  t.equal(actual, expected);
+  t.end();
+});
---
+++ week5/1122-unit-testing/01-apples/apples.ts
@@ -0,0 +1,21 @@
+// Create an object, with one method (eg. getApple()) that returns a string (eg. 'apple')
+// Create a test for that:
+// Create a test method
+// Instantiate an Object from your class in the method
+// Use the t.equal()
+// The expected parameter should be the same string (eg. 'apple')
+// The actual parameter should be the return value of the method (eg. myobject.getApple())
+// Run the test
+// Change the expected value to make the test failing
+// Run the test
+// Fix the returned value to make the test succeeding 
+
+export class Apple {
+  getApple(): string {
+    return 'apple';
+  }
+}
+
+let goldenApple = new Apple;
+
+console.log(goldenApple.getApple());  
