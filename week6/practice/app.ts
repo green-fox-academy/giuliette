5f893bbcac3c424ed6bf4a6b5b6b6c334edbaed0
giuliette <domanyjuli@gmail.com>
2018-11-29 12:27:56 +0100

    feat(wanderer): app.ts added

---
+++ week6/practice/app.ts
@@ -0,0 +1,11 @@
+import { CarPark } from "./carpark";
+import { Car } from './car'
+
+let nepliget = new CarPark();
+// let auto = new Car(0, '6776bb', 8999);
+
+// nepliget.addCarToFile(auto, 'cars.csv');
+
+nepliget.addCarToFile(new Car(0, 'ABC-333', 2000), 'cars.csv');
+
+nepliget.removeCar('ABC-333', 'cars.csv');
