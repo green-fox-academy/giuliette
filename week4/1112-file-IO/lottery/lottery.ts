2dd42203626a31108452e1491c200cd812ea7358
giuliette <domanyjuli@gmail.com>
2018-12-04 12:53:35 +0100

    feat(Lottery): remove the most occurences from all the numbers

--- week4/1112-file-IO/lottery/lottery.ts
+++ week4/1112-file-IO/lottery/lottery.ts
@@ -1,43 +1,40 @@
 'use strict'
-export {};
-let fs = require ('fs');
+export { };
+let fs = require('fs');
 
 function readFromFile(fileName: string): string {
-    try {
-      return fs.readFileSync(fileName, 'utf-8');
-    } catch(e) {
-      console.log(e.message);
-      return null;
-    }
+  try {
+    return fs.readFileSync(fileName, 'utf-8');
+  } catch (e) {
+    console.log(e.message);
+    return null;
   }
+}
 
-  function checkNumbers(fileName: string) {
-    const fileContent = readFromFile(fileName);
-    if (fileContent !== null) {
-      let line = fileContent.split('\n');
-      let lotteryNumbers: string[][] = [];
-      let commonNumbers = {};
+function checkNumbers(fileName: string) {
+  const fileContent = readFromFile(fileName);
+  if (fileContent !== null) {
+    let line = fileContent.split('\n');
+    let lotteryNumbers: string[][] = [];
+    let commonNumbers = {};
 
-      for (let i = 0; i < line.length; i++) {
-        lotteryNumbers.push(line[i].split(';').splice(-5, 5));
-        //console.log(lotteryNumbers);
-      }
+    for (let i = 0; i < line.length; i++) {
+      lotteryNumbers.push(line[i].split(';').splice(-5, 5));
+      //console.log(lotteryNumbers);
+    }
 
-      for (let i = 0; i < lotteryNumbers.length; i++) {
-        for (let j = 0; j < lotteryNumbers[i].length; j++) {
-          //console.log(lotteryNumbers[i][j]);
-          if(commonNumbers[lotteryNumbers[i][j]] !== undefined) { //undefined = false-nak felel meg.
-            (commonNumbers[lotteryNumbers[i][j]])++;
-          } else {
-            (commonNumbers[lotteryNumbers[i][j]]) =1;
-          }
+    for (let i = 0; i < lotteryNumbers.length; i++) {
+      for (let j = 0; j < lotteryNumbers[i].length; j++) {
+        //console.log(lotteryNumbers[i][j]);
+        if (commonNumbers[lotteryNumbers[i][j]] !== undefined) { //undefined = false-nak felel meg.
+          (commonNumbers[lotteryNumbers[i][j]])++;
+        } else {
+          (commonNumbers[lotteryNumbers[i][j]]) = 1;
         }
       }
-      Object.keys(commonNumbers).sort(function(a,b) {
-        return commonNumbers[a] - commonNumbers[b];
-      })
     }
+    return Object.keys(commonNumbers).sort((a, b) => commonNumbers[b] - commonNumbers[a]).splice(0, 5);
   }
+}
 
-  checkNumbers('lottery.csv');
-  
+console.log(checkNumbers('lottery.csv'));
