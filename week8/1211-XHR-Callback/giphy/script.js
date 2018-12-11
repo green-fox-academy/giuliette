207c543713cd1d4ad89da0c3c20ca04944d55ee8
Juli Domány <domanyjuli@gmail.com>
2018-12-11 14:37:47 +0100

    feat(Giphy): objects called

---
+++ week8/1211-XHR-Callback/giphy/script.js
@@ -0,0 +1,18 @@
+let URL = 'https://api.giphy.com/v1/gifs/search?api_key=4J8GNFWQJWN8juUDnveKoSGbyDaKKG78&q=cat&limit=15&offset=0&rating=G&lang=en';
+
+const sendHTTPRequest = (url, method, callback) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      // http check is done, we can work with the parsed data
+      //console.log(JSON.parse(xhr.responseText).data);
+      callback(JSON.parse(xhr.responseText).data);
+    }
+  }
+  xhr.send();
+}
+
+sendHTTPRequest(URL, 'GET', (response) => {
+  console.log(response);
+});
