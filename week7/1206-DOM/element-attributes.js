f1c7141f72a9c3e040933d71e8b701475306ab66
Juli Domány <domanyjuli@gmail.com>
2018-12-12 13:32:39 +0100

    feat(DOM): element attributes

---
+++ week7/1206-DOM/element-attributes.js
@@ -0,0 +1,5 @@
+Element attributes
+const littlePrince = document.querySelector('img');
+
+console.log(littlePrince.getAttribute('src'));
+littlePrince.setAttribute('src', 'http://deji.chez.com/dessins/pp1.gif');
