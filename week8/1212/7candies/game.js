4024857f431aff625a472cbe068823c243ec4977
Juli Domány <domanyjuli@gmail.com>
2018-12-12 17:41:24 +0100

    feat(candyShop): undone

--- week8/1212/7candies/game.js
+++ week8/1212/7candies/game.js
@@ -1,12 +1,25 @@
 // Clicking the ‘Create candies’ button gives you 1 candy.
-const buttonCreateCandies = document.querySelector('.create-candies');
-const candiesNumber = document.querySelector('.candies');
-
-let number = 0;
-
-let callbackCreateCandy = () => {
-  number++; 
-  candiesNumber.innerText = `${number}`;
+let candyCount = 0;
+const createCandy = document.querySelector('.create-candies');
+const candyNum = document.querySelector('.candies');
+let createCandy = () => {
+  candyCount++; 
+  candyNum.innerText = `${candyCount}`;
 }
 
-buttonCreateCandies.addEventListener('click', callbackCreateCandy);
+createCandy.addEventListener('click', createCandy);
+
+// You can buy a lollipop for 100 candies by clicking the ‘Buy lollipop’ button.
+let lollyPopCounter = 0;
+const buyLollypops = document.querySelector('.buy-lollypops');
+const lollypop = document.querySelector('.lollypops');
+const exchangeRate = document.querySelector('.speed');
+
+let buyLollipops = ('click', event => {
+  if (candyCount >= 10) {
+    candyCount -= 10;
+    lollyPopCounter++;
+    lollypop.innerText += `🍭`;
+  }
+}
+
