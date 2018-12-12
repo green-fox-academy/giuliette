097a35f2caa51b5848fe0680feae2311d4fdd10c
Juli Domány <domanyjuli@gmail.com>
2018-12-12 14:22:18 +0100

    feat(bubbling): task

---
+++ week8/1212/4bubbling/script.js
@@ -0,0 +1,8 @@
+
+//   Here is an image inspector, but the buttons are not implemented yet, that will
+//   be your task!
+//    - the nav buttons (up, down, left, right) move the background by 10px
+//    - the zoom buttons increase/decrease the image by 20%
+//    - attach only two event listeners to the nav element
+//      - one for navigation
+//      - one for zooming
