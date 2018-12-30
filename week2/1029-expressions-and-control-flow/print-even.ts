46da2e7e948a7c8aa7ed6de41a0c03fd682d1de6
Juli Domány <domanyjuli@gmail.com>
2018-12-30 14:43:21 +0100

    fix(repostructure): week2

--- week2/1029-expressions-and-control-flow/print-even.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-export{};
-
-// Create a program that prints all the even numbers between 0 and 500
-
-for (let j: number = 1; j <= 500; j++) {
-    if(j % 2 === 0)
-    console.log(j);
-  }
