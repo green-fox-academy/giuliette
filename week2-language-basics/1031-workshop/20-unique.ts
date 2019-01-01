4ab08e34aeb4a43f200ebf573b7d35ec2a0b37a2
Juli Domány <domanyjuli@gmail.com>
2019-01-01 12:05:54 +0100

    feat(repostructure): unique solution added

--- week2-language-basics/1031-workshop/20-unique.ts
+++ week2-language-basics/1031-workshop/20-unique.ts
@@ -4,12 +4,17 @@ export { };
 //  Create a function that takes a list of numbers as a parameter
 //  Returns a list of numbers where every number in the list occurs only once
 
-function unique(arr) {
-
-}
-
 //  Example
 console.log(unique([1, 11, 34, 11, 52, 61, 1, 34]))
 //  should print: `[1, 11, 34, 52, 61]`
 
-  
+function unique(arr: number[]): number[] {
+  for (let i: number = 0; i < arr.length; i++) {
+    for (let j: number = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        arr.splice(j, 1);
+      }
+    }
+  }
+  return arr;
+}
